668bf0c9b35d1a6e091831462bd8486130deae92
Alexandre Salvador de Oliveira <alexandre.salvador.ext@dasa.com.br>
2024-04-15 16:15:57 -0300

    Add cards api

---
+++ cards-api/src/main/java/dev/alexandreoliveira/microservices/cardsapi/CardsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.alexandreoliveira.microservices.cardsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsApiApplication.class, args);
+	}
+
+}
---
+++ cards-api/src/test/java/dev/alexandreoliveira/microservices/cardsapi/CardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandreoliveira.microservices.cardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
