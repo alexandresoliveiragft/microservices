a416d4b53f6e582bd75727f4bc5d069c1e4171f6
Alexandre Salvador de Oliveira <alexandre.salvador.ext@dasa.com.br>
2024-04-18 15:25:21 -0300

    Add config-api

---
+++ config-api/src/test/java/dev/alexandreoliveira/microservices/configapi/ConfigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandreoliveira.microservices.configapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
