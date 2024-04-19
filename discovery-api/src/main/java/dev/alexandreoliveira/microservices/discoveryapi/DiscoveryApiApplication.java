964411cb6ffd9280820a8f86f1e1909b7c5483ff
Alexandre Salvador de Oliveira <alexandre.salvador.ext@dasa.com.br>
2024-04-19 14:56:25 -0300

    add reload config by actuator, add discovery-api for discovery servers.

---
+++ discovery-api/src/main/java/dev/alexandreoliveira/microservices/discoveryapi/DiscoveryApiApplication.java
@@ -0,0 +1,15 @@
+package dev.alexandreoliveira.microservices.discoveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApiApplication.class, args);
+	}
+
+}
---
+++ discovery-api/src/test/java/dev/alexandreoliveira/microservices/discoveryapi/DiscoveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandreoliveira.microservices.discoveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
